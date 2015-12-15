c6fe657392233be2e8a71b564c86a757514df963
Raghav Kumar Gautam <raghav@apache.org>
2015-12-15 01:23:37 -0800

    simplifying client interface

--- src/main/scala/Js2Java.scala
+++ src/main/scala/Js2Java.scala
@@ -1,51 +1,55 @@
 import scala.collection.mutable
 
-class Js2Java(json: String) {
-  val classes = mutable.ListBuffer[String]()
-
-  private def getType(kv: (String, Any)): String = {
-    val key: String = kv._1
-    val value: Any = kv._2
-    value match {
-      case s: Seq[Any] =>
-        val className = "SomeClass"
-        s"$className[]"
-      case s: Map[String, Any] =>
-        val className = key.capitalize
-        genClass(s, className)
-        s"$className"
-      case _ => value.getClass.getSimpleName
-    }
-  }
-  private def genFields(props: Map[String, Any]): Iterable[String] = {
-    props.map { kv =>
-      val fieldName: String = kv._1
-      val typ: Any = getType(kv)
-      val fieldHeaderTemplate = "@SerializedName(\"%s\")"
-      val fieldTemplate: String = "final %s %s;"
-      fieldHeaderTemplate.format(fieldName) + "\n" +
-        fieldTemplate.format(typ, fieldName) + "\n"
-    }
-  }
-  private def genClass(props: Map[String, Any], className: String): String = {
-    val classStart: String = s"class $className {"
-    val classBody: String = genFields(props).reduce(_ + "\n" + _)
-    val classEnd: String = "}"
-    s"$classStart\n$classBody$classEnd\n"
+object js2Java {
+  def generate(jsonStr: String, clsName: String) = {
+    new Js2Java(jsonStr, clsName).generate
   }
 
-  def parseJsValue(parsed: Any, className: String): String = {
-    val clsNtyp: (String, String) = parsed match {
-      case props: Map[String, Any] => (genClass(props, className), className)
-      case propsArr: Array[Map[String, Any]] => (genClass(propsArr.head, s"oneOf$className"), s"oneOf$className[]")
-    }
-    classes.append(clsNtyp._1)
-    clsNtyp._2
-  }
+  private class Js2Java(json: String, className: String) {
+    val classes = mutable.ListBuffer[(String, String)]()
 
-  def generate: List[String] = {
-    val parsed: Any = scala.util.parsing.json.JSON.parseFull(json).get
-    parseJsValue(parsed, "JsRoot")
-    classes.toList
+    private def getType(kv: (String, Any)): String = {
+      val key: String = kv._1
+      val value: Any = kv._2
+      value match {
+        case s: Seq[Any] =>
+          parseArrOrMap(value, key.capitalize)
+        case s: Map[String, Any] =>
+          parseArrOrMap(value, key.capitalize)
+        case _ => value.getClass.getSimpleName
+      }
+    }
+    private def genFields(props: Map[String, Any]): Iterable[String] = {
+      props.map { kv =>
+        val fieldName: String = kv._1
+        val typ: Any = getType(kv)
+        val fieldHeaderTemplate = "@SerializedName(\"%s\")"
+        val fieldTemplate: String = "final %s %s;"
+        fieldHeaderTemplate.format(fieldName) + "\n" +
+          fieldTemplate.format(typ, fieldName) + "\n"
+      }
+    }
+    private def genClass(props: Map[String, Any], className: String): String = {
+      val classStart: String = s"class $className {"
+      val classBody: String = genFields(props).reduce(_ + "\n" + _)
+      val classEnd: String = "}"
+      s"$classStart\n$classBody$classEnd\n"
+    }
+
+    def parseArrOrMap(parsed: Any, className: String): String = {
+      val clsNameDef: (String, String) = parsed match {
+        case props: Map[String, Any] => (className, genClass(props, className))
+        case propsArr: Seq[Map[String, Any]] => (s"oneOf$className[]", genClass(propsArr.head, s"oneOf$className"))
+        case _ => throw new Exception(parsed.toString)
+      }
+      classes.append(clsNameDef)
+      clsNameDef._1
+    }
+
+    def generate: List[(String, String)] = {
+      val parsed: Any = scala.util.parsing.json.JSON.parseFull(json).get
+      parseArrOrMap(parsed, className)
+      classes.reverse.toList
+    }
   }
 }
--- src/test/scala/SimpleTest.scala
+++ src/test/scala/SimpleTest.scala
@@ -8,18 +8,23 @@ class SimpleTest {
 
   @DataProvider(name= "jsonProvider")
   def jsonProvider(): Array[Array[Object]] = {
-    val simpleJson = this.getClass.getResource("simple.json").getFile
-    val resourceDir: File = new File(simpleJson).getParentFile
-    val jsonFilter: Array[String] = Array("json")
-    val jsonFiles = FileUtils.listFiles(resourceDir, jsonFilter, false)
-    val jsonStrings = jsonFiles.asScala.map(io.Source.fromFile(_).mkString).toList
-    val ret: List[Array[Object]] = jsonStrings.map(_.asInstanceOf[Object]).map(Array(_))
-    ret.toArray
+    def getJsonFiles: Array[File] = {
+      val simpleJson = this.getClass.getResource("simple.json").getFile
+      val resourceDir: File = new File(simpleJson).getParentFile
+      val jsonFilter: Array[String] = Array("json")
+      val jsonFiles = FileUtils.listFiles(resourceDir, jsonFilter, false).asScala.toList
+      jsonFiles.toArray
+    }
+    def asObject(s: String) = s.asInstanceOf[Object]
+    val jsonFiles: Array[File] = getJsonFiles
+    val jsonFileNames = jsonFiles.map(_.getName.split('.').head.capitalize).map(asObject)
+    val jsonStrings = jsonFiles.map(io.Source.fromFile(_).mkString).map(asObject)
+    jsonStrings.zip(jsonFileNames).map(p => Array(p._1, p._2))
   }
 
   @Test(dataProvider = "jsonProvider")
-  def testJs2Java(jsStr:String) = {
-    val javaStr = js2Java.generate(jsStr, "JsRoot")
+  def testJs2Java(jsStr:String, clsName: String) = {
+    val javaStr = js2Java.generate(jsStr, clsName)
     println(jsStr)
     javaStr.map(_._2).foreach(println)
   }
