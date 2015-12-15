013ceafd2fa58b18e651935efb4b567d1b1e831d
Raghav Kumar Gautam <raghav@apache.org>
2015-12-15 11:52:16 -0800

    read arg from commandline

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,5 +13,28 @@ object Main extends App {
       |  }
       |}}
     """.stripMargin
-  println(js2Java.generate(data, "JsRoot"))
+  args.length match {
+    case 0 =>
+      println("No arg provided. Using demo data...")
+      println(data)
+      println(js2Java.generate(data, "JsRoot"))
+      println("Please supply a json file name or url as argument.")
+    case 1 =>
+      val arg: String = args.head
+      if (arg.startsWith("http://") || arg.startsWith("https://")) {
+        val data = io.Source.fromURL(arg).mkString
+        printJava(js2Java.generate(data, "JsRoot"), s"Invalid json fetched from url: $arg\n${data.take(500)}")
+      } else {
+        println("Need to handle this case.")
+      }
+
+  }
+
+  def printJava(java: List[(String, String)], message: String): Unit = {
+    try {
+      java.map(_._2).foreach(println)
+    } catch {
+      case ex: Throwable => println(message)
+    }
+  }
 }
