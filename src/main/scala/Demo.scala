a9f501d136826e59c2b9e7a30dab3de21b245869
Raghav Kumar Gautam <raghav@apache.org>
2016-03-19 19:58:33 -0700

    adding python demo

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,6 +1,11 @@
 object Demo extends App {
   Template.setTemplates(Util.getResource("demo.vm"), null)
   val jsStr = io.Source.fromURL("https://api.github.com/users/raghavgautam").mkString
-  val scalaStr = js2Code.generate(jsStr, "GithubUser").map(_.toString).reduce(_ + "\n" + _)
-  print(scalaStr)
+  val scalaCode = js2Code.generate(jsStr, "GithubUser").map(_.toString).reduce(_ + "\n" + _)
+  println("*"*10 + " SCALA CODE " + "*"*10)
+  println(scalaCode)
+  println("*"*10 + " PYTHON CODE " + "*"*10)
+  Template.setTemplates(Util.getResource("python-demo.vm"), null)
+  val pythonCode = js2Code.generate(jsStr, "GithubUser").map(_.toString).reduce(_ + "\n" + _)
+  print(pythonCode)
 }
